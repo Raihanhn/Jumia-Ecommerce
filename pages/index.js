003fa85f68d369868bd636fd71647a6e561018b9
Raihan <raihanhn20@gmail.com>
2023-11-09 23:14:04 +0600

    Header complete

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+import Header from "@/components/header/header";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="md:max-w-7xl mx-auto p-2">
+      <Header />
+    </main>
+  );
 }
